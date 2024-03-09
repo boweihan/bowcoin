796bf5a83e1bbeec31e1d12db555235f3ca7f2d3
bowei <bowei.han100@gmail.com>
2024-03-08 20:42:47 -0500

    chain validation

--- main.js
+++ main.js
@@ -18,3 +18,50 @@ class Block {
     ).toString();
   }
 }
+
+class Blockchain {
+  constructor() {
+    // must start with a genesis block
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "01/01/2017", "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    // set linked list pointers
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    // don't worry about block 0, it's the genesis block
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      // recalculate the hash to see if it matches
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+let bowCoin = new Blockchain();
+
+bowCoin.addBlock(new Block(1, "10/07/2017", { amount: 4 }));
+bowCoin.addBlock(new Block(2, "12/07/2017", { amount: 10 }));
+
+console.log(JSON.stringify(bowCoin, null, 4));
